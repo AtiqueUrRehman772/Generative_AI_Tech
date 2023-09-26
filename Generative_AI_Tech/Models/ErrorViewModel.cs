42cf10b30e9a9615181ecf90da189977ae992b55
Atique Ur Rehman <74912009+AtiqueUrRehman772@users.noreply.github.com>
2023-09-26 08:08:52 +0500

    Add project files.

---
+++ Generative_AI_Tech/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Generative_AI_Tech.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
